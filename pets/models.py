a9e8e43eae187c6f23ffb7cbf9809c53a4e822ea
danmatuoka <dan_mtk@hotmail.com>
2023-01-02 17:57:14 -0300

    feat: model pet

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class typeOptions(models.TextChoices):
+    CAT = "Cat"
+    DOG = "Dog"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    type = models.CharField(max_length=20, choices=typeOptions.choices)
+    age = models.CharField(max_length=50, null=False, blank=False)
+    neutered = models.BooleanField()
+    vaccinated = models.BooleanField()
+    docile = models.BooleanField()
+
+    user_id = models.ForeignKey("users.user", on_delete=models.CASCADE, related_name="pets")
+
+
+#    def __repr__(self) -> str:
+#        return f"<Pet [{self.id}] - {self.name}>"
