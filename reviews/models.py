7868ceed75df35feb3a5d338c395ee9e5e1e5c76
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-02 15:30:27 -0300

    feat: add review models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Reviews(models.Model):
+    class Meta: 
+        ordering = ["id"]
+
+    review_text = models.CharField(max_length=255)
+    stars = models.PositiveIntegerField(max_value=5)
+    reservation = models.ManyToManyField('reservations.Reservation', on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="reviews")
