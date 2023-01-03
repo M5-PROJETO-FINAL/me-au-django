4a2d62338c0d0b6b6c668f9dd90b799a8110b3da
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-03 15:44:42 -0300

    fix: fixing validations

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 import uuid
 
 class Reviews(models.Model):
     
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     review_text = models.CharField(max_length=255)
-    stars = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    stars = models.PositiveIntegerField()
     reservation = models.ManyToManyField('reservations.Reservation', related_name="reviews")
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="reviews")
