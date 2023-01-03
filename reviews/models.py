d2ad8a53252ebd13997bc5d440c83e0e48838219
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-03 08:55:05 -0300

    fix: add uuid in the id

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+import uuid
 
 class Reviews(models.Model):
-    class Meta: 
-        ordering = ["id"]
-
+    
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     review_text = models.CharField(max_length=255)
-    stars = models.PositiveIntegerField(max_value=5)
-    reservation = models.ManyToManyField('reservations.Reservation', on_delete=models.CASCADE, related_name="reviews")
+    stars = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+    reservation = models.ManyToManyField('reservations.Reservation', related_name="reviews")
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="reviews")
