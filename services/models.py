193c800ca9e7953b4afd242ba367407b0e16e7a5
nicholas <nicholas.ge1301@gmail.com>
2023-01-03 09:58:06 -0300

    fix: rooms urls

--- services/models.py
+++ services/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class Service(models.Model):
-    name = models.CharField(unique=True)
-    description = models.CharField()
+    name = models.CharField(max_length=10, unique=True)
+    description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __repr__(self) -> str:
         return f"Service [{self.id}]"
-
