adfd83e4f6e1abfde73a990db034cd91267c92a9
nicholas <nicholas.ge1301@gmail.com>
2023-01-04 10:39:11 -0300

    feat: successful user creation test

--- tests/users/test_users_view.py
+++ tests/users/test_users_view.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase
+from users.models import User
 
 # from django.test import TestCase
 from rest_framework.views import status
@@ -31,3 +32,41 @@ class UserViewTest(APITestCase):
         returned_fields = set(resulted_data.keys())
         msg = "Verifique se todas as chaves obrigatórias são retornadas ao tentar criar um usuário sem dados"
         self.assertSetEqual(expected_fields, returned_fields, msg)
+
+    def test_admin_user_creation(self):
+        user_data = {
+            "name": "Nicholas",
+            "email": "nicholas@mail.com",
+            "password": "1234",
+            "is_adm": True,
+        }
+
+        # STATUS CODE
+        response = self.client.post(self.BASE_URL, data=user_data, format="json")
+        expected_status_code = status.HTTP_201_CREATED
+        result_status_code = response.status_code
+        msg = (
+            "Verifique se o status code retornado do POST "
+            + f"em `{self.BASE_URL}` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+        # RETORNO JSON
+        expected_data = {
+            "id": 1,
+            "name": "Nicholas",
+            "email": "nicholas@mail.com",
+            "is_adm": True,
+            "profile_img": None,
+            "cpf": None,
+        }
+        resulted_data = response.json()
+        msg = (
+            "Verifique se as informações do usuário retornada no POST "
+            + f"em `{self.BASE_URL}` estão corretas."
+        )
+        self.assertDictEqual(expected_data, resulted_data, msg)
+
+        added_user = User.objects.get(id=1)
+        msg = "Verifique se o password foi hasheado corretamente"
+        self.assertTrue(added_user.check_password(user_data["password"]), msg)
