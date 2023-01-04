33c3983f913ec8e74e67d2b62e01383482a085b9
nicholas <nicholas.ge1301@gmail.com>
2023-01-04 09:41:09 -0300

    feat: reservation creation

--- pets/views.py
+++ pets/views.py
@@ -4,7 +4,7 @@ from .models import Pet
 from .serializers import PetSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrPetOwner
-
+import ipdb
 
 
 class PetView(ListCreateAPIView):
@@ -14,16 +14,14 @@ class PetView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-
     def get_queryset(self):
         if self.request.user.is_adm:
             return self.queryset.all()
 
-        return self.queryset.filter(user_id=self.request.user)
-
+        return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user_id=self.request.user)
+        serializer.save(user=self.request.user)
 
 
 class PetDetailView(RetrieveUpdateDestroyAPIView):
--- rooms/aux_functions/dates.py
+++ rooms/aux_functions/dates.py
@@ -1,6 +1,34 @@
-def get_dates_in_range(checkin, checkout):
-    return []
+from datetime import datetime, timedelta
+from typing import List
 
 
-def are_dates_conflicting(checkin1, checkout1, checkin2, checkout2):
-    return False
+def get_dates_in_range(min_date: datetime, max_date: datetime) -> List[datetime]:
+    """
+    Returns an array of dates with all days spanning a given range, starting from minDate and ending on maxDate (inclusive)
+
+    Example:
+
+    getDatesInRange('2010-01-20', '2010-01-24')
+    returns:
+    ['2010-01-20', '2010-01-21', '2010-01-22', '2010-01-23', '2010-01-24']
+    (except that dates are actually Date objects instead of strings)
+    """
+    dates = []
+    curr_date = min_date
+    while curr_date < max_date:
+        dates.append(curr_date)
+        curr_date += timedelta(1)
+    return dates
+
+
+def are_dates_conflicting(
+    checkin1: datetime, checkout1: datetime, checkin2: datetime, checkout2: datetime
+):
+    """
+    Returns true if the time interval specified by the first pair of parameters is somehow conflicting with the time interval specified by the second pair of parameters.
+    """
+    return (
+        (checkout2 > checkin1 and checkout2 <= checkout1)
+        or (checkin2 >= checkin1 and checkin2 < checkout1)
+        or (checkin2 <= checkin1 and checkout2 >= checkout1)
+    )
