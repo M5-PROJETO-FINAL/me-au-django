3af28a0f148ffb04fb85e2587d94c3117d9d1a6a
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-04 16:37:07 -0300

    fix: serializers in user and reservation

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
-from django.core.validators import MaxValueValidator, MinValueValidator
 from .models import Reviews
 from users.serializers import UserSerializer
+from reservations.serializers import ReservationSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
 
     stars = serializers.IntegerField(max_value=5, min_value=1)
     user = UserSerializer(read_only=True)
+    reservation = ReservationSerializer()
+
     class Meta:
         model = Reviews
         fields = ['id', 'review_text', 'stars', 'reservation', 'user']
-        read_only_fields = ['reservation', 'user']
+        read_only_fields = ['user']
         
 
