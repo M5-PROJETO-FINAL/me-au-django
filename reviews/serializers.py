d065a7431fd87a0abd005d4424d2b4e4494efd54
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-04 11:32:37 -0300

    fix: correcting return error

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from django.core.validators import MaxValueValidator, MinValueValidator
 from .models import Reviews
-# from users.serializers import UserSerializer
+from users.serializers import UserSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
 
     stars = serializers.IntegerField(max_value=5, min_value=1)
-
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Reviews
-        fields = ['id', 'review_text', 'stars', 'reservation_id', 'user']
-        read_only_fields = ['reservation_id', 'user']
+        fields = ['id', 'review_text', 'stars', 'reservation', 'user']
+        read_only_fields = ['reservation', 'user']
         
 
