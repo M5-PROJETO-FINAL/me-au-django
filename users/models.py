e950aec6c288cebeb3e5ae62c5b1f9ec4e3953a2
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-05 14:16:14 -0300

    fix: add uuid in users

--- users/models.py
+++ users/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-
+import uuid
 from .manager import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=127)
     email = models.EmailField(unique=True, max_length=255, blank=False)
     is_adm = models.BooleanField(default=False, null=True, blank=True)
