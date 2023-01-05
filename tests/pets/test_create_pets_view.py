d49328d22e37919c8770d37d80c7c3ba9fffde26
danmatuoka <dan_mtk@hotmail.com>
2023-01-05 20:24:26 -0300

    feat: tests pet view

---
+++ tests/pets/test_create_pets_view.py
@@ -0,0 +1,126 @@
+from rest_framework.test import APITestCase
+from pets.models import Pet
+from users.models import User
+from tests.factories import create_user_with_token, create_normal_user_with_token
+
+# from django.test import TestCase
+from rest_framework.views import status
+import ipdb
+
+
+class PetViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.BASE_URL = "/api/pets/"
+
+        cls.user_1_super, token_1 = create_user_with_token()
+        cls.access_token_1 = str(token_1.access_token)
+
+        cls.user_2_normal, token_2 = create_normal_user_with_token()
+        cls.access_token_2 = str(token_2.access_token)
+
+        cls.pet_data = {
+            "name": "Kitty",
+            "type": "cat",
+            "age": "2 years old",
+            "neutered": True,
+            "vaccinated": True,
+            "docile": True
+        }
+
+
+    def test_pet_creation_without_token(self):
+
+        response = self.client.post(self.BASE_URL, data=self.pet_data, format="json")
+
+        # STATUS CODE
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+        resulted_status_code = response.status_code
+        msg = (
+            "Verifique se o status code retornado do POST sem todos os campos obrigatórios "
+            + f"em `{self.BASE_URL}` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, resulted_status_code, msg)
+
+        # RETORNO JSON
+        expected_data = {"detail": "Authentication credentials were not provided."}
+        resulted_data = response.json()
+        msg = (
+            "Verifique se os dados retornados do POST sem token "
+            + f"em `{self.BASE_URL}` é {expected_data}"
+        )
+        self.assertDictEqual(expected_data, resulted_data, msg)
+
+
+    def test_pet_creation_with_wrong_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
+        response = self.client.post(self.BASE_URL, data=self.pet_data, format="json")
+
+        # STATUS CODE
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+        resulted_status_code = response.status_code
+        msg = (
+            "Verifique se o status code retornado do POST sem todos os campos obrigatórios "
+            + f"em `{self.BASE_URL}` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, resulted_status_code, msg)
+
+        # RETORNO JSON
+        expected_data = {"detail": "Given token not valid for any token type",
+	                        "code": "token_not_valid",
+	                        "messages": [
+		                        {
+		                        	"token_class": "AccessToken",
+		                        	"token_type": "access",
+		                        	"message": "Token is invalid or expired"
+		                        }
+	                    ]}
+        resulted_data = response.json()
+
+        msg = (
+            "Verifique se os dados retornados do POST com token invalido "
+            + f"em `{self.BASE_URL}` é {expected_data}"
+        )
+        self.assertDictEqual(expected_data, resulted_data, msg)
+
+
+    def test_non_admin_user_creation(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token_1)
+        response = self.client.post(self.BASE_URL, data=self.pet_data, format="json")
+        pet_db = Pet.objects.last()
+
+        # STATUS CODE
+        expected_status_code = status.HTTP_201_CREATED
+        result_status_code = response.status_code
+        msg = (
+            "Verifique se o status code retornado do POST "
+            + f"em `{self.BASE_URL}` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+        
+
+        # RETORNO JSON
+        expected_data = {
+            "id": str(pet_db.id),
+            "name": "Kitty",
+            "type": "cat",
+            "age": "2 years old",
+            "neutered": True,
+            "vaccinated": True,
+            "docile": True,
+            "user":{
+                "id": str(pet_db.user.id),
+		        "name": "User Test",
+                "email": "user_test@mail.com",
+                "is_adm": True,
+		        "profile_img": None,
+		        "cpf": None
+            }
+        }
+
+        resulted_data = response.json()
+        msg = (
+            "Verifique se as informações do pet retornada no POST "
+            + f"em `{self.BASE_URL}` estão corretas."
+        )
+        self.assertDictEqual(expected_data, resulted_data, msg)
