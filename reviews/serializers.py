c70bb53fa212655fc37026559aaf203fed8dec65
Jasmynels <jasmyneleandro0@gmail.com>
2023-01-05 11:06:46 -0300

    fix: reservation serializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -8,11 +8,11 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     stars = serializers.IntegerField(max_value=5, min_value=1)
     user = UserSerializer(read_only=True)
-    reservation = ReservationSerializer()
+    # reservation_id = ReservationSerializer()
 
     class Meta:
         model = Reviews
-        fields = ['id', 'review_text', 'stars', 'reservation', 'user']
-        read_only_fields = ['user']
+        fields = ["id", "review_text", "stars", "reservation", "user"]
+        read_only_fields = ["user"]
         
 
