13a90a499ea6dd9a2edfe3cdceb51551d056d42c
danmatuoka <dan_mtk@hotmail.com>
2023-01-06 10:01:28 -0300

    add new test in pet create view

--- tests/pets/test_create_pets_view.py
+++ tests/pets/test_create_pets_view.py
@@ -28,6 +28,14 @@ class PetViewTest(APITestCase):
             "docile": True
         }
 
+        cls.pet_data_2 = {
+            "name": "Dog",
+            "type": "dog",
+            "age": "2 years old",
+            "neutered": True,
+            "vaccinated": True,
+        }
+
 
     def test_pet_creation_without_token(self):
 
@@ -66,25 +74,18 @@ class PetViewTest(APITestCase):
         self.assertEqual(expected_status_code, resulted_status_code, msg)
 
         # RETORNO JSON
-        expected_data = {"detail": "Given token not valid for any token type",
-	                        "code": "token_not_valid",
-	                        "messages": [
-		                        {
-		                        	"token_class": "AccessToken",
-		                        	"token_type": "access",
-		                        	"message": "Token is invalid or expired"
-		                        }
-	                    ]}
+        expected_data = "Given token not valid for any token type"
         resulted_data = response.json()
+        resulted_data_message = str(resulted_data['detail'])
 
         msg = (
             "Verifique se os dados retornados do POST com token invalido "
             + f"em `{self.BASE_URL}` é {expected_data}"
         )
-        self.assertDictEqual(expected_data, resulted_data, msg)
+        self.assertEqual(expected_data, resulted_data_message, msg)
 
 
-    def test_non_admin_user_creation(self):
+    def test_pet_creation(self):
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token_1)
         response = self.client.post(self.BASE_URL, data=self.pet_data, format="json")
         pet_db = Pet.objects.last()
@@ -97,25 +98,52 @@ class PetViewTest(APITestCase):
             + f"em `{self.BASE_URL}` é {expected_status_code}"
         )
         self.assertEqual(expected_status_code, result_status_code, msg)
-        
 
         # RETORNO JSON
         expected_data = {
             "id": str(pet_db.id),
-            "name": "Kitty",
-            "type": "cat",
-            "age": "2 years old",
-            "neutered": True,
-            "vaccinated": True,
-            "docile": True,
+            "name": pet_db.name,
+            "type": pet_db.type,
+            "age": pet_db.age,
+            "neutered": pet_db.neutered,
+            "vaccinated": pet_db.vaccinated,
+            "docile": pet_db.docile,
             "user":{
                 "id": str(pet_db.user.id),
-		        "name": "User Test",
-                "email": "user_test@mail.com",
-                "is_adm": True,
-		        "profile_img": None,
-		        "cpf": None
+		        "name": pet_db.user.name,
+                "email": pet_db.user.email,
+                "is_adm": pet_db.user.is_adm,
+		        "profile_img": pet_db.user.profile_img,
+		        "cpf": pet_db.user.cpf
             }
+        }
+
+        resulted_data = response.json()
+        msg = (
+            "Verifique se as informações do pet retornada no POST "
+            + f"em `{self.BASE_URL}` estão corretas."
+        )
+        self.assertDictEqual(expected_data, resulted_data, msg)
+
+
+    def test_pet_creation_with_missing_field(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token_1)
+        response = self.client.post(self.BASE_URL, data=self.pet_data_2, format="json")
+
+        # STATUS CODE
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        result_status_code = response.status_code
+        msg = (
+            "Verifique se o status code retornado do POST "
+            + f"em `{self.BASE_URL}` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+        # RETORNO JSON
+        expected_data = {
+	        "docile": [
+		        "This field is required."
+	        ]
         }
 
         resulted_data = response.json()
