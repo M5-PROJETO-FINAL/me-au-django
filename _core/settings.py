51c4557033954f2a074fd8cd73d561ededafbfe4
nicholas <nicholas.ge1301@gmail.com>
2023-01-10 12:09:33 -0300

    updated production settings

--- _core/settings.py
+++ _core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ec2-34-205-127-26.compute-1.amazonaws.com"]
 
