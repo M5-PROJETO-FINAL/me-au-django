950c7e50b94b9d855e826b4d62dac18ce21ac66e
nicholas <nicholas.ge1301@gmail.com>
2023-01-10 12:17:38 -0300

    fix: review permissions

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework import permissions
-from users.models import User
+from reviews.models import Reviews
 from rest_framework.views import View
 
 
 class IsAccountOwnerReview(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj: User) -> bool:
+    def has_object_permission(self, request, view: View, obj: Reviews) -> bool:
         return request.user.is_authenticated and obj.user == request.user
